5b5c5d1c4ee5d0469540bac591de9588ed5f2993
David Bowers <dnlbowers@gmail.com>
2024-02-10 00:32:41 +0100

    set up and register reducers

--- src/products/store/actions/pizzas.action.ts
+++ src/products/store/actions/pizzas.action.ts
@@ -8,16 +8,16 @@ export const LOAD_PIZZAS_FAIL = '[Products] Load Pizzas Fail';
 export const LOAD_PIZZAS_SUCCESS = '[Products] Load Pizzas Success';
 
 export class LoadPizzas implements Action {
-    readonly types = LOAD_PIZZAS;
+    readonly type = LOAD_PIZZAS;
 }
 
 export class LoadPizzasFail implements Action {
-    readonly types = LOAD_PIZZAS_FAIL;
+    readonly type = LOAD_PIZZAS_FAIL;
     constructor(public payload: any) {}
 }
 
 export class LoadPizzasSuccess implements Action {
-    readonly types = LOAD_PIZZAS_SUCCESS;
+    readonly type = LOAD_PIZZAS_SUCCESS;
     constructor(public payload: Pizza[]) {}
 }
 
