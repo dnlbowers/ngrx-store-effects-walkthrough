94b976d96e4109d649690f4a275e4585c6dc568d
David Bowers <dnlbowers@gmail.com>
2024-02-11 01:33:57 +0100

    implement first ngrx effect

--- src/products/store/reducers/pizzas.reducer.ts
+++ src/products/store/reducers/pizzas.reducer.ts
@@ -2,58 +2,15 @@ import * as fromPizzas from '../actions/pizzas.action'
 
 import { Pizza } from '../../models/pizza.model';
 export interface PizzaState {
-    data: Pizza[],
-    loaded: boolean,
-    loading: boolean
+    data: Pizza[];
+    loaded: boolean;
+    loading: boolean;
 };
 
 export const initialState: PizzaState = {
-    data: [
-        {
-            "name": "Seaside Surfin'",
-            "toppings": [
-                {
-                    "id": 6,
-                    "name": "mushroom"
-                },
-                {
-                    "id": 7,
-                    "name": "olive"
-                },
-                {
-                    "id": 2,
-                    "name": "bacon"
-                },
-                {
-                    "id": 3,
-                    "name": "basil"
-                },
-                {
-                    "id": 1,
-                    "name": "anchovy"
-                },
-                {
-                    "id": 8,
-                    "name": "onion"
-                },
-                {
-                    "id": 11,
-                    "name": "sweetcorn"
-                },
-                {
-                    "id": 9,
-                    "name": "pepper"
-                },
-                {
-                    "id": 5,
-                    "name": "mozzarella"
-                }
-            ],
-            "id": 2
-        },
-    ],
+    data: [],
     loaded: false,
-    loading: false
+    loading: false,
 };
 
 export function reducer(
@@ -69,10 +26,12 @@ export function reducer(
             }
         }
         case fromPizzas.LOAD_PIZZAS_SUCCESS: {
+            const data = action.payload;
             return {
                 ...state,
                 loading: false,
-                loaded: true
+                loaded: true,
+                data,
             }
         }
         case fromPizzas.LOAD_PIZZAS_FAIL: {
